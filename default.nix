ca18a69e416fd21155cc158e7d1429fab0108c16
Jakub Kozłowski <kubukoz@gmail.com>
2021-01-18 22:22:50 +0100

    Better syntax

--- default.nix
+++ default.nix
@@ -1,18 +1,21 @@
+{ pkgsBuilder ? import <nixpkgs> }:
+
 let
-  overlays = let
-    graalvm = self: super:
-      let
-        jre = self.callPackage ./graalvm { };
-        jdk = jre;
-      in {
-        inherit jre jdk;
-        scala = super.scala.override { inherit jre; };
+  pkgs = pkgsBuilder {
+    overlays = let
+      graalvm = self: super:
+        let
+          jre = self.callPackage ./graalvm { };
+          jdk = jre;
+        in {
+          inherit jre jdk;
+          scala = super.scala.override { inherit jre; };
+        };
+      bloop = self: super: {
+        bloop = self.callPackage ./derivations/bloop.nix {
+          version = "1.4.6-15-209c2a5c";
+        };
       };
-    bloop = self: super: {
-      bloop = self.callPackage ./derivations/bloop.nix {
-        version = "1.4.6-15-209c2a5c";
-      };
-    };
-  in [ graalvm bloop ];
-  pkgs = import <nixpkgs> { inherit overlays; };
+    in [ graalvm bloop ];
+  };
 in with pkgs; [ jdk sbt scala ammonite scalafmt coursier bloop ]
